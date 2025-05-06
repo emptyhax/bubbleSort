470e08476b7b9c38d2ccaa77a054cd10dd3b58d6
haxbash <bluemaquinajp@gmail.com>
2025-05-06 17:44:59 -0300

    fix: create temp variable for swapping elements in bubble sort

---
+++ bubbleSort.cr
@@ -0,0 +1,2 @@
+def bubbleSort(arr : Array(Int32)) : Array(Int32)
+
--- bubbleSort.cs
+++ bubbleSort.cs
@@ -1,14 +1,13 @@
 using System;
 
 class Program {
-
   static void bubbleSort(int[] arr) {
     for (int i=0; i < arr.Length - 1; i++) {
       for (int j=0; j < arr.Length - i - 1; j++) {
         if (arr[j] > arr[j + 1]) {
           int temp = arr[j];
           arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
+          arr[j + 1] =e:;
         }
       }
     }
