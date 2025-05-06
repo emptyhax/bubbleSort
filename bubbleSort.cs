10e94e1d71628c83fe69f8b89de53be2aee2aac5
haxbash <bluemaquinajp@gmail.com>
2025-05-06 00:58:52 -0300

    bubbleSort [C#]

---
+++ bubbleSort.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program {
+
+  static void bubbleSort(int[] arr) {
+    for (int i=0; i < arr.Length - 1; i++) {
+      for (int j=0; j < arr.Length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+  }
+  static void Main(string[] args) {
+
+    int[] arr = { 1, 22, 94, 1, 14, 23, 9, 7, 104, 74, 0, 4, 64, 34, 91, 44, 53 };
+    bubbleSort(arr);
+    Console.WriteLine(string.Join(", ", arr));
+  }
+}
+
