9c31539b63a51c9aae9b9cda498a5ce981015415
haxbash <bluemaquinajp@gmail.com>
2025-05-06 00:48:16 -0300

    bubbleSort [C++]: 4.0 (fixed all)

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int[] arr, int n) {
+void bubbleSort(int arr[], int n) {
   for (int i = 0; i < n - 1; i++) {
     for (int j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
@@ -11,18 +11,20 @@ void bubbleSort(int[] arr, int n) {
       }
     }
   }
+}
 
-  void printArray(int[] arr, int n) {
-    for (int i = 0; i < n; i++) {
-      cout << arr[i] << " ";
-    }
-    cout << endl
+void printArray(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
   }
+  cout << endl;
+}
 
-  int main() {
-    int arr[] = {1, 22, 94, 1, 14, 23, 9, 7, 104, 74, 0, 4, 64, 34, 91, 44, 53};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, n) printArray(arr, n) return 0
-  }
+int main() {
+  int arr[] = {1, 22, 94, 1, 14, 23, 9, 7, 104, 74, 0, 4, 64, 34, 91, 44, 53};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  bubbleSort(arr, n);
+  printArray(arr, n);
+return 0;
 }
 
